00676815c39910e16ae54276af4bb28ab033ca93
aichenovo <3809933253@qq.com>
2026-01-28 21:09:49 +0800

    Add files via upload

---
+++ lib/bean/card/bangumi_info_card.dart
@@ -0,0 +1,264 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:kazumi/bean/widget/collect_button.dart';
+import 'package:kazumi/utils/constants.dart';
+import 'package:kazumi/modules/bangumi/bangumi_item.dart';
+import 'package:kazumi/bean/card/network_img_layer.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+// 视频卡片 - 水平布局
+class BangumiInfoCardV extends StatefulWidget {
+  const BangumiInfoCardV({
+    super.key,
+    required this.bangumiItem,
+    required this.isLoading,
+  });
+
+  final BangumiItem bangumiItem;
+  final bool isLoading;
+
+  @override
+  State<BangumiInfoCardV> createState() => _BangumiInfoCardVState();
+}
+
+class _BangumiInfoCardVState extends State<BangumiInfoCardV> {
+  int touchedIndex = -1;
+
+  Widget get voteBarChart {
+    final int votes = widget.bangumiItem.votes;
+    final List<int> votesCount = widget.bangumiItem.votesCount;
+    final int denom = votes <= 0 ? 1 : votes;
+    return Flexible(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '  评分透视:',
+          ),
+          SizedBox(height: 16),
+          AspectRatio(
+            aspectRatio: 2,
+            child: BarChart(
+              duration: Duration(milliseconds: 80),
+              BarChartData(
+                // alignment: BarChartAlignment.spaceEvenly,
+                borderData: FlBorderData(show: false),
+                gridData: FlGridData(show: false),
+                barTouchData: BarTouchData(
+                  touchCallback: (FlTouchEvent event, barTouchResponse) {
+                    setState(() {
+                      if (!event.isInterestedForInteractions ||
+                          barTouchResponse == null ||
+                          barTouchResponse.spot == null) {
+                        touchedIndex = -1;
+                        return;
+                      }
+                      touchedIndex =
+                          barTouchResponse.spot!.touchedBarGroupIndex;
+                    });
+                  },
+                  touchTooltipData: BarTouchTooltipData(
+                    getTooltipColor: (_) =>
+                        Theme.of(context).colorScheme.inverseSurface,
+                    getTooltipItem: (group, groupIndex, rod, rodIndex) {
+                      final percentage = (votesCount[groupIndex] / denom) * 100;
+                      return BarTooltipItem(
+                        '${percentage.toStringAsFixed(2)}% (${votesCount[groupIndex]}人)',
+                        TextStyle(
+                            color:
+                                Theme.of(context).colorScheme.onInverseSurface),
+                      );
+                    },
+                  ),
+                ),
+                barGroups: List<BarChartGroupData>.generate(
+                  10,
+                  (i) => BarChartGroupData(
+                    x: i + 1,
+                    barRods: [
+                      BarChartRodData(
+                        toY: votesCount[i].toDouble(),
+                        color: touchedIndex == i
+                            ? Theme.of(context).colorScheme.primary
+                            : Theme.of(context).disabledColor,
+                        width: 20,
+                        borderRadius:
+                            BorderRadius.vertical(top: Radius.circular(5)),
+                      )
+                    ],
+                    // showingTooltipIndicators: [0],
+                  ),
+                ),
+                titlesData: FlTitlesData(
+                  bottomTitles: AxisTitles(
+                    sideTitles: SideTitles(
+                      showTitles: true,
+                      reservedSize: 30,
+                      getTitlesWidget: (value, meta) => SideTitleWidget(
+                        meta: meta,
+                        space: 10,
+                        child: Text(value.toInt().toString()),
+                      ),
+                    ),
+                  ),
+                  topTitles: const AxisTitles(),
+                  leftTitles: const AxisTitles(),
+                  rightTitles: const AxisTitles(),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final bool canShowVoteBarChart =
+        !widget.isLoading &&
+        widget.bangumiItem.votesCount.length >= 10 &&
+        widget.bangumiItem.votesCount.any((v) => v > 0);
+
+    return Container(
+      height: 300,
+      constraints: BoxConstraints(maxWidth: 950),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            widget.bangumiItem.nameCn == ''
+                ? widget.bangumiItem.name
+                : (widget.bangumiItem.nameCn),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          SizedBox(height: 16),
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Flexible(
+                  child: AspectRatio(
+                    aspectRatio: 0.65,
+                    child: LayoutBuilder(builder: (context, boxConstraints) {
+                      final double maxWidth = boxConstraints.maxWidth;
+                      final double maxHeight = boxConstraints.maxHeight;
+                      return Hero(
+                        transitionOnUserGestures: true,
+                        tag: widget.bangumiItem.id,
+                        child: NetworkImgLayer(
+                          src: widget.bangumiItem.images['large'] ?? '',
+                          width: maxWidth,
+                          height: maxHeight,
+                          fadeInDuration: const Duration(milliseconds: 0),
+                          fadeOutDuration: const Duration(milliseconds: 0),
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+                SizedBox(width: 16),
+                Flexible(
+                  child: Skeletonizer(
+                    enabled: widget.isLoading,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '首播日期:',
+                            ),
+                            Text(
+                              widget.bangumiItem.airDate == ''
+                                  ? '2000-11-11' // Skeleton Loader 占位符
+                                  : widget.bangumiItem.airDate,
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Theme.of(context).colorScheme.primary,
+                              ),
+                            ),
+                            SizedBox(height: 8),
+                            Text(
+                              '${widget.bangumiItem.votes} 人评分:',
+                            ),
+                            if (widget.isLoading)
+                              // Skeleton Loader 占位符
+                              Text(
+                                '10.0 ********',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Theme.of(context).colorScheme.primary,
+                                ),
+                              ),
+                            if (!widget.isLoading)
+                              Row(
+                                children: [
+                                  Text(
+                                    '${widget.bangumiItem.ratingScore}',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 8),
+                                  RatingBarIndicator(
+                                    itemCount: 5,
+                                    rating: widget.bangumiItem.ratingScore
+                                            .toDouble() /
+                                        2,
+                                    itemBuilder: (context, index) => Icon(
+                                      Icons.star_rounded,
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                    ),
+                                    itemSize: 20.0,
+                                  ),
+                                ],
+                              ),
+                            SizedBox(height: 8),
+                            Text(
+                              'TMDB ID:',
+                            ),
+                            Text(
+                              '${widget.bangumiItem.id}',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Theme.of(context).colorScheme.primary,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 120,
+                          height: 40,
+                          child: CollectButton.extend(
+                            bangumiItem: widget.bangumiItem,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                if (MediaQuery.sizeOf(context).width >=
+                        LayoutBreakpoint.compact['width']! &&
+                    canShowVoteBarChart)
+                  voteBarChart,
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
